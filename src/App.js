64ba2a97e15a8398a91bba2652efc4296838c9e6
uladzimir <01myit@gmail.com>
2023-08-08 20:11:48 +0300

    my first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './table-of-contents-svgrepo-com.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='logo'><img src={logo} className="App-logo" alt="logo" /></div>
+        <h1>Table with JSON data</h1>
+      </header>
+    </div>
+  ) 
+}
+
+export default App 
