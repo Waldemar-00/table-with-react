71806234c0773ee9aa5b9cd22a630063f013e943
uladzimir <01myit@gmail.com>
2023-08-09 21:32:58 +0300

    added a data file, made a table layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './table-of-contents-svgrepo-com.svg'
 import './App.css'
+// import {data} from './data'
 
 function App() {
   return (
@@ -8,6 +9,48 @@ function App() {
         <div className='logo'><img src={logo} className="App-logo" alt="logo" /></div>
         <h1>Table with JSON data</h1>
       </header>
+      <main className='Store'>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th colspan='2'>Fruit</th>
+            </tr>
+            <tr>
+              <td>Apple</td>
+              <td>1$</td>
+            </tr>
+            <tr>
+              <td>Dragonfriut</td>
+              <td>1$</td>
+            </tr>
+            <tr>
+              <td>Passionfruit</td>
+              <td>2$</td>
+            </tr>
+            <tr>
+              <th colspan='2'>Vegetables</th>
+            </tr>
+            <tr>
+              <td>Spinach</td>
+              <td>2$</td>
+            </tr>
+            <tr>
+              <td>Pumpkin</td>
+              <td>4$</td>
+            </tr>
+            <tr>
+              <td>Peas</td>
+              <td>2$</td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
     </div>
   ) 
 }
